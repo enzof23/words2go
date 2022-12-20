e7eb98cf25135ce6a458e44b1bb9e4b1d2974004
enzo filippo <enzo.filippo23@gmail.com>
2022-12-20 16:23:48 +0100

    practice writeadded

--- src/components/practice/Write.tsx
+++ src/components/practice/Write.tsx
@@ -1,11 +1,239 @@
+import { useEffect, useState } from "react";
 import { WordType } from "../../types/list_types";
+import { FiDelete } from "react-icons/fi";
+import { createAnswerArr, getRandomWord } from "../../utils/util";
+import { Link } from "react-router-dom";
 
-type CompParams = {
-  words: WordType[];
+type Letter = {
+  letter: string;
+  selected: boolean;
+  id: string;
 };
 
-const Write = ({ words }: CompParams) => {
-  return <div>Write</div>;
+const Write = ({ words }: { words: WordType[] }) => {
+  /* States */
+
+  // Copy of words array to keep track of words that have been tested already
+  const [wordsList, setWordsList] = useState<WordType[]>(words);
+
+  // Get a random word from wordsList to display for practice
+  const [question, setQuestion] = useState<WordType>(getRandomWord(words));
+
+  // Get the answer (translation) from question state
+  const [answer, setAnswer] = useState<Letter[]>([]);
+
+  // Track if user has gone through whole list
+  const [listFinished, setListFinished] = useState<boolean>(false);
+
+  // Don't know button handler
+  const [showAnswer, setShowAnswer] = useState<boolean>(false);
+
+  // Display the answer from letters selected
+  const [answerDisplay, setAnswerDisplay] = useState<Letter[]>([]);
+  const answerSelected = answerDisplay.map((letter) => letter.letter).join("");
+
+  // Check Answer
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+
+  /* States End */
+  /*  */
+
+  /* Functions */
+
+  useEffect(() => {
+    const answerArr = createAnswerArr(
+      question.translation,
+      getRandomWord(words).translation
+    );
+
+    setAnswer(answerArr);
+  }, [question, wordsList]);
+
+  // Letter buttons click handler
+  const cardClickHandler = (item: Letter) => {
+    setIsCorrect(null);
+
+    // Map through answer (letters) array and update selected keys to disable button
+    const newAnswer = answer.map((le) => {
+      if (le.id === item.id) return { ...le, selected: true };
+      else return le;
+    });
+
+    setAnswer(newAnswer);
+    setAnswerDisplay((prev) => [...prev, item]);
+  };
+
+  // Delete icon handler to delete last char selected
+  const deleteAnswer = () => {
+    // toggle selected key back to false for delete char
+    const lastIndex = answerDisplay.length - 1;
+    const lastLetterID = answerDisplay[lastIndex].id;
+
+    const newAnswer = answer.map((le) => {
+      if (le.id === lastLetterID) return { ...le, selected: false };
+      else return le;
+    });
+
+    setAnswer(newAnswer);
+
+    const newAnswerDisplay = answerDisplay.slice(0, -1);
+    setAnswerDisplay(newAnswerDisplay);
+
+    setIsCorrect(null);
+  };
+
+  const checkAnswer = () => {
+    if (answerSelected === question.translation) {
+      setIsCorrect(true);
+
+      setShowAnswer(true);
+    } else {
+      setIsCorrect(false);
+    }
+  };
+
+  // Get next word - remove latest word from wordsList and update question
+  const getNextWord = () => {
+    const newWordsList = wordsList.filter(
+      (word) => word.wordID !== question.wordID
+    );
+
+    if (newWordsList.length > 0) {
+      setWordsList(newWordsList);
+
+      setQuestion(getRandomWord(newWordsList));
+
+      setShowAnswer(false);
+
+      setAnswerDisplay([]);
+
+      setIsCorrect(null);
+    } else {
+      setListFinished(true);
+    }
+  };
+
+  // Start over after list is finished
+  const startAgain = () => {
+    setWordsList(words);
+
+    setQuestion(getRandomWord(words));
+
+    setListFinished(false);
+
+    setShowAnswer(false);
+
+    setAnswerDisplay([]);
+
+    setIsCorrect(null);
+  };
+
+  /* Functions End */
+
+  /*  */
+
+  return (
+    <>
+      {!listFinished ? (
+        <div className="write__container">
+          {/* Question Container */}
+          <div className="question__container">
+            <div className="question__word">{question.word}</div>
+            {!showAnswer ? (
+              <div
+                className="question__button"
+                onClick={() => setShowAnswer(!showAnswer)}
+              >
+                Don't know ?
+              </div>
+            ) : isCorrect ? (
+              <p className="correct__msg">correct !</p>
+            ) : null}
+          </div>
+          {/* Question Container End */}
+
+          {/*  */}
+
+          {/* Divider */}
+          <div className="divider" />
+
+          {/* Answer Container */}
+          <div className="answer__container">
+            {/* Answer Input */}
+            {!showAnswer ? (
+              <div
+                className={`answer__display ${answerSelected && "has-answer"} ${
+                  isCorrect === false && "incorrect"
+                }`}
+              >
+                <div className="word__display">
+                  {!answerSelected ? (
+                    <p>Start typing your answer</p>
+                  ) : (
+                    answerSelected
+                  )}
+                </div>
+                <button
+                  className="delete__button"
+                  onClick={deleteAnswer}
+                  disabled={!answerSelected}
+                >
+                  <FiDelete />
+                </button>
+                <button
+                  className={`answer__button ${isCorrect === false && "shake"}`}
+                  disabled={!answerSelected}
+                  onClick={checkAnswer}
+                >
+                  answer
+                </button>
+              </div>
+            ) : (
+              // Display answer and "next" btn when user clicks on "don't know ?" btn
+              <div
+                className={`answer__display has-answer ${
+                  isCorrect === true ? "correct" : "incorrect"
+                }`}
+              >
+                <div className="word__display">{question.translation}</div>
+                <button className="answer__button" onClick={getNextWord}>
+                  next
+                </button>
+              </div>
+            )}
+
+            {/* Answer Options */}
+            <div className="answer__options">
+              {answer.map((item) => {
+                const { letter, selected, id } = item;
+                return (
+                  <button
+                    key={id}
+                    className="option__card"
+                    disabled={selected || showAnswer}
+                    onClick={() => cardClickHandler(item)}
+                  >
+                    {letter}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* Answer Container End */}
+        </div>
+      ) : (
+        <div className="write__container">
+          <div className="list-empty__message">
+            You have reached the end of the list ! <br /> What's next ?
+          </div>
+          <div className="list-empty__buttons">
+            <Link to={"/practice"}>Go back to Library</Link>
+            <button onClick={startAgain}>Start again</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Write;
--- src/utils/util.tsx
+++ src/utils/util.tsx
@@ -1,7 +1,56 @@
+import { WordType } from "../types/list_types";
+
 export const getRandomId = () => {
   return (
     Date.now().toString(36) + Math.random().toString(36).split(".").join("")
   );
+};
+
+export const getRandomIndexInArray = (length: number) => {
+  return Math.floor(Math.random() * length);
+};
+
+export const getRandomWord = (list: WordType[]) => {
+  const index = getRandomIndexInArray(list.length);
+  return list[index];
+};
+
+export const createAnswerArr = (answer: string, word2: string) => {
+  const MAX_LENGTH = answer.length > 4 ? 2 : 4;
+  const randomIndex = getRandomIndexInArray(word2.length);
+  const sliceLength =
+    randomIndex > 4 ? MAX_LENGTH : randomIndex === 0 ? 1 : randomIndex;
+  const randomLetters = word2.slice(0, sliceLength);
+
+  const shuffledWord = randomizeLetters(answer.concat(randomLetters));
+
+  const answerArr = shuffledWord.split("").map((letter) => ({
+    letter,
+    selected: false,
+    id: getRandomId(),
+  }));
+
+  return answerArr;
+};
+
+export const randomizeLetters = (word: string) => {
+  let wordArray = word.split("");
+
+  // Keep shuffling the letters until the shuffled word is different from the original word
+  let shuffledWord = word;
+  while (shuffledWord === word) {
+    // Shuffle the letters using the Fisher-Yates shuffle algorithm
+    for (let i = wordArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+
+    // Convert the shuffled array back to a word
+    shuffledWord = wordArray.join("");
+  }
+
+  // Return the shuffled word
+  return shuffledWord;
 };
 
 export const getDate = () => {
